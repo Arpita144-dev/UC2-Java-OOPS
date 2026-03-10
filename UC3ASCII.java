891bc1407c7d39b1aa46a4982c523682933f2e2c
arpita <arpita@arpitas-MacBook-Air-331.local>
2026-03-11 00:16:23 +0530

    added 3 more uc

---
+++ UC3ASCII.java
@@ -0,0 +1,13 @@
+public class UC3ASCII {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     *"));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", " ***** "));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "      *"));
+        System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*     *"));
+        System.out.println(String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "));
+
+    }
+}
